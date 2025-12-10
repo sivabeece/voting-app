695820e9a1b6532d2988955ee37984d03d3defc0
sivabeece <siva4ubusiness@gmail.com>
2025-12-10 23:12:03 +0530

    fix: fine tuned code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('voting-app');
+  menus = signal([
+    { id: 1, name: 'Add User' , route:'/', icon:'fa-solid fa-users'},
+    { id: 2, name: 'Cast Vote' , route:'/castvote', icon: 'fa-solid fa-person-chalkboard'},
+  ]);
+  activeItem = signal<number>(1);
+
+  setActive(item: number) {
+    this.activeItem.set(item);
+  }
 }
--- src/app/pages/castvote/castvote.ts
+++ src/app/pages/castvote/castvote.ts
@@ -1,111 +1,104 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { signal, Component, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { AllVoters, VoterService } from '../../shared/service/voter.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [CardModule], // Ensure DropDownModule is correctly imported
+  imports: [CardModule, FormsModule],
   templateUrl: './castvote.html',
   styleUrl: './castvote.css',
+  standalone: true,
 })
-export class CastVote  implements OnInit{
-  selectedValue: number | null = null;
-  selectedVoter: any = null;
-  allUsers: AllVoters[] = [];
+export class CastVote implements OnInit {
+  selectedCandidate: number | null = null; // Set Selected Candidate
+  selectedVoter: any = null; // Set Selected Voter
+  allUsers: AllVoters[] = []; // All Users
 
-  votersData: AllVoters[] = [];
-  candidateData: AllVoters[] = [];
-  candidates: any[] = [];
-  voters: any[] = [];
-  votes: any;
-  
-  radioOptions = [
-    { value: 'option1', label: 'Option 1' },
-    { value: 'option2', label: 'Option 2' },
-    { value: 'option3', label: 'Option 3' }
-  ];
+  votersData: AllVoters[] = []; // Filter volters alone
+  candidateData: AllVoters[] = []; // Filter candidates alone
+  candidates = signal<{ value: number; label: string }[]>([]); // Candidates for dropdown
+  voters = signal<{ id: number; name: string; hasVoted: boolean }[]>([]); // Voters for dropdown
+  votes: any; // Votes of selected candidate
 
   constructor(private voterService: VoterService) { }
 
   ngOnInit() {
-     this.fetchAllUsers();
+    this.fetchAllUsers(); // Fetch all users
   }
 
+  // Fetch all users from the service
   fetchAllUsers() {
     this.voterService.getAllUsers().subscribe((data: AllVoters[]) => {
-      setTimeout(() => {
-        console.log('Fetched Users:', data);
-        
-        this.votersData = data.filter((user: AllVoters) => user.userType === 2);
-        this.candidateData = data.filter((user: AllVoters) => user.userType === 1);
 
-          this.candidates = this.candidateData.map(c => ({
-            label: c.voterName,
-            value: c.voterId
-          }));
+      this.votersData = data.filter((user: AllVoters) => user.userType === 2);
+      this.candidateData = data.filter((user: AllVoters) => user.userType === 1);
 
-          this.voters = this.votersData.map(v => ({
-            name: v.voterName, id: v.voterId, hasVoted: v.hasVoted
-          }));
+      this.candidates.set(this.candidateData.map(c => ({
+        label: c.voterName,
+        value: c.voterId
+      })));
 
-      }, 500);
-      
+      this.voters.set(this.votersData.map(v => ({
+        name: v.voterName, id: v.voterId, hasVoted: v.hasVoted
+      })));
+
+      console.log(this.voters);
+
     }, (error) => {
       console.error('Error fetching Sites', error);
     });
   }
 
+  // Handle candidate selection and casting the vote
   onSelectCandidate(value: number) {
-      if (!this.selectedVoter) {
-        alert("Please select a voter first.");
-        this.selectedValue = null;
-        this.votes = null;
-        return;
-      }
+    if (!this.selectedVoter) {
+      alert("Please select a voter first.");
+      this.selectedCandidate = null;
+      this.votes = null;
+      return;
+    }
 
-      this.votes = this.candidateData.find(candidate => candidate.voterId === value);
-    
-      console.log("Selected voter:", this.selectedVoter);
-      console.log("Selected candidate object:", this.votes);
-      this.selectedValue = value;
-      console.log("Selected candidate value:", value);
-    
-      const votePayload = {
-        candidateId: value,
-        voterId: this.selectedVoter.id,
-        votes: this.votes.votes + 1
-      };
-    
-      this.voterService.castVote(votePayload).subscribe({
-        next: (response) => {
-          alert("Vote cast successfully!");
-    
-          // Refresh cleanly
-          this.selectedVoter = null;
-          this.selectedValue = null;
-          this.votes = null;
-    
-          // Re-fetch users cleanly
-          this.fetchAllUsers();
-        },
-        error: (error) => {
-          console.error("Error casting vote", error);
-          alert("Failed to cast vote.");
-        }
-      });
-    
-    
+    this.votes = this.candidateData.find(candidate => candidate.voterId === value);
+    this.selectedCandidate = value;
+
+    const votePayload = {
+      candidateId: value,
+      voterId: this.selectedVoter.id,
+      votes: this.votes.votes + 1
+    };
+
+    this.voterService.castVote(votePayload).subscribe({
+      next: (response) => {
+        alert("Vote cast successfully!");
+
+        // Refresh cleanly
+        this.selectedVoter = null;
+        this.selectedCandidate = null;
+        this.votes = null;
+
+        // Re-fetch users cleanly
+        this.fetchAllUsers();
+      },
+      error: (error) => {
+        console.error("Error casting vote", error);
+        alert("Failed to cast vote.");
+      }
+    });
+
+
   }
 
+  // Handle voter selection and promts alert when the selected voter already voted
   OnSelectVoter(vote: any) {
-    this.selectedValue = vote.name;
-    if(vote.hasVoted) {
+    this.selectedCandidate = vote.name;
+    if (vote.hasVoted) {
       alert('This voter has already cast their vote.');
       this.selectedVoter = null;
       return;
     } else {
-     
+
       this.selectedVoter = vote;
     }
-    this.selectedValue = null;
+    this.selectedCandidate = null;
   }
 }
